44d31b0ee326a8ff60ac73ef87a8962822831ba1
abir270 <jammeliabir@gmail.com>
2026-04-19 21:02:05 +0100

    fix supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+const supabaseUrl = 'https://dyjsggqdugfgbtryycjq.supabase.co'
+const supabaseKey = 'sb_publishable_YGiBzHtltVvdXqwFj66a0Q_q0qg3Xcr'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
